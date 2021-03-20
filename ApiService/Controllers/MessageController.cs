79b6f3d3648ff216fc817ae28014344e29a71446
Валерий Рахманов <dalamar95@gmail.com>
2021-03-20 22:47:23 +0300

    Commint before test

---
+++ ApiService/Controllers/MessageController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiService.Model;
+
+namespace ApiService.Controllers
+{
+    [Route("API/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private MessageRepository _messageRepository = MessageRepository.GetMessageRepository();
+
+        [HttpGet]
+        public MessageRepository Get() => _messageRepository;
+
+        [HttpPost]
+        public void Post([FromBody] Message msg)
+        {
+            msg.Date = DateTime.Now.ToString();
+            _messageRepository.AddMessage(msg);
+        }
+    }
+}
--- ApiService/Startup.cs
+++ ApiService/Startup.cs
@@ -25,7 +25,7 @@ namespace ApiService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
         }
 
@@ -40,6 +40,8 @@ namespace ApiService
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(builder => builder.AllowAnyOrigin());
 
             app.UseAuthorization();
 
