3431422e0b71904679408b365a927e835007d78c
Валерий Рахманов <dalamar95@gmail.com>
2021-03-24 03:30:38 +0300

    Add EF support

---
+++ ApiService/DataAccessLayer/WebApiDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiService.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+namespace ApiService.DataAccessLayer
+{
+    public class WebApiDBContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+        public WebApiDBContext(DbContextOptions<WebApiDBContext> options) : base(options)
+        {
+        }
+    }
+}
--- ApiService/Model/Message.cs
+++ ApiService/Model/Message.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ApiService.Model
 {
+    [Table("Messages")]
     public class Message
     {
-        public string UserName { get; set; }
-        public string Text { get; set; }
+        [Key]
+        [Required]
         public int ID { get; set; }
+
+        [Required]
+        public int RoomID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [StringLength(16)]
+        public string UserName { get; set; }
+
+        [StringLength(256)]
+        public string Text { get; set; }
     }
 }
