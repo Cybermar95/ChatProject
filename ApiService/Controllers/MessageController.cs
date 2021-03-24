ac220c3848d2e859680ceaa59fab12a126ae1205
Валерий Рахманов <dalamar95@gmail.com>
2021-03-24 19:07:12 +0300

    Add DI

--- ApiService/Controllers/MessageController.cs
+++ ApiService/Controllers/MessageController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ApiService.Model;
+using ApiService.DataAccessLayer;
 
 namespace ApiService.Controllers
 {
@@ -11,17 +12,17 @@ namespace ApiService.Controllers
     [ApiController]
     public class MessageController : ControllerBase
     {
-        private MessageRepository _messageRepository = MessageRepository.GetMessageRepository();
+        private readonly IMessageService _businessLayer;
 
-        [HttpGet ("{id}")]
-        public IEnumerable<Message> Get(int id) => _messageRepository.GetMessage(id);
+        public MessageController(IMessageService businessLayer)
+        {
+            _businessLayer = businessLayer;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Message> Get(int id) => _businessLayer.GetMessages(id);
 
         [HttpPost]
-        public Message Post([FromBody] Message message)
-        {
-            message.ID = _messageRepository.GetNewId();
-            _messageRepository.AddMessage(message);
-            return message;
-        }
+        public Message Post([FromBody] Message message) => _businessLayer.AddMessage(message);
     }
 }
--- ApiService/Startup.cs
+++ ApiService/Startup.cs
@@ -27,8 +27,7 @@ namespace ApiService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<WebApiDBContext>(options => options.UseSqlite(Configuration.GetConnectionString("cs")));
-
+            services.AddDB(Configuration);
             services.AddCors();
             services.AddControllers();
         }
